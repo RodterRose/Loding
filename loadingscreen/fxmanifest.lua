96dc943ca127fe91381dd4f9493df3af7f9da2b4
RodterRose <128921415+RodterRose@users.noreply.github.com>
2023-03-27 19:25:48 +0530

    Add files via upload

---
+++ loadingscreen/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes' 
+ description 'Moon-V LoadingScreen'
+
+version '0.0.1'
+
+loadscreen 'ladescreen.html'
+files {
+	'*.png',
+	'Zenci.mp3',
+	'**/*.png',
+	'**/*.css', 
+	'fonts/MARK-BOOK-ITALIC.TTF',
+	'fonts/FALENA-MEDIUMITALIC.OTF',
+	'fonts/RANY-MEDIUM.OTF',
+	'fonts/MARK-BOOK.TTF',
+	'**/*.js',
+	'ladescreen.html'
+}
+
+
